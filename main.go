50a4bc0e00ce57f8166f7dd76cb6caa0efe94316
ardszsantos <lokgamer01@gmail.com>
2024-11-11 14:31:04 -0300

    developing

--- main.go
+++ main.go
@@ -2,16 +2,47 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("************ Welcome to the CLI Password Generator ************")
 	menu()
+
+	decisao := readDecision()
+
+	switch decisao {
+	case 1:
+		var passwordLength int
+		fmt.Scan("How long do you want the password to be? ", &passwordLength)
+
+	case 2:
+		fmt.Println("Exibindo Logs....")
+	case 3:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
 }
 
 func menu() {
-	fmt.Println("Select the desired option: ")
 
-	var passwordLength int
-	fmt.Scan("How long do you want the password to be? ", &passwordLength)
+	fmt.Scan("Select the desired option: ")
+
+	fmt.Println("1- Generate Password")
+	fmt.Println("2- Check Generated Passwords")
+	fmt.Println("3- Exit")
+	fmt.Println(" ")
+
+}
+
+func readDecision() int {
+	var menuDecision int
+	fmt.Scan(&menuDecision)
+	fmt.Println("O comando escolhido foi", menuDecision)
+
+	return menuDecision
 }
